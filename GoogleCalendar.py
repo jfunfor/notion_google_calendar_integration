df5d65708340874da5a7f1d2a5c664914f4a37ca
Egor <e.v.kuklin1998@gmail.com>
2024-07-13 00:11:17 +0300

    fix: add more logs to create event func

--- GoogleCalendar.py
+++ GoogleCalendar.py
@@ -54,9 +54,10 @@ class GoogleCalendar(object):
     def create_event(self, event, calendarId):
         e = self.service.events().insert(calendarId=calendarId,
                                          body=event).execute()
-        print('Event created: %s' % (e.get('id')))
+        print('Event created in Google calendar with id: %s' % (e.get('id')))
+        print('event body:', event)
 
     def delete_event(self, eventId, calendarId):
         self.service.events().delete(calendarId=calendarId,
                                      eventId=eventId).execute()
-        print('Event deleted: %s' % eventId)
+        print('Event deleted in Google calendar with id: %s' % eventId)
